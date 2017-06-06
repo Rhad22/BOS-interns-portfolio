13069c327b5e1b4aeb550f55632611ec6a22e7db
BOS INTERNS <BOS INTERNS>
2017-06-06 13:42:27 +0800

    webpage

---
+++ js/animate.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+// Push the body and the nav over by 285px over
+  var main = function() {
+    $('.fa-bars').click(function() {
+      $('.nav-screen').animate({
+        right: "0px"
+      }, 200);
+
+      $('body').animate({
+        right: "285px"
+      }, 200);
+    });
+}
